83bcc08288da1f578cd66bdf3b208679f88932fb
Bogdan Langhe <bogdanlanghe@gmail.com>
2023-10-25 09:48:40 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "rgba(36, 5, 62, 1)",
+        eucalyptus: "rgba(68, 255, 161, 1)",
+        "davys-grey": "rgba(88, 77, 98, 1)",
+        "ghost-white": "rgba(252, 248, 255, 1)",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+        fraunces: ["Fraunces", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
